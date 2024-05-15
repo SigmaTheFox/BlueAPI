14d2fd5a0d6c0c997f9c5904f3bb3ef3099e4db0
masaya-jkl <34386898+masaya-jkl@users.noreply.github.com>
2024-05-16 01:49:34 +0200

    Implemented RegEx parsing for: school, group, role, damage type, armor type, combat class & affinities.

--- index.js
+++ index.js
@@ -13,15 +13,54 @@ function getStudent() {
 
 	const {
 		groups: { studentProfileImg },
-	} =
-		/data-title=\"Profile\s?Image[\"><a-z\s=\-:\/\.]*src=\"\/\/(?<studentProfileImg>[a-zA-Z\.\/0-9-_]*)/gim.exec(
-			text
-		);
+	} = /data-title=\"Profile\s?Image[\"><a-z\s=\-:\/\.]*src=\"\/\/(?<studentProfileImg>[a-zA-Z\.\/0-9-_]*)/gim.exec(
+		text
+	);
+	
+	const {
+		groups: { studentSchool, studentGroup, studentRole },
+	} = /(?:<th class=\"character-school character-header\">.*?<td title=\")(?<studentSchool>[^,]*?)(?:,\s*)(?<studentGroup>[^\"]*)(?:\">)(?:.*?<td>.*?<\/p>\s*)(?<studentRole>.*?)(?:<\/td>)/gis.exec(
+		text
+	);
+	
+	const {
+		groups: { studentDamageType },
+	} = /(?:<tr><th>Damage Type<\/th>\s*<td.*?>)(?<studentDamageType>[^<]*)(?:<\/td>)/gis.exec(text);
+
+	const {
+		groups: { studentArmorType },
+	} = /(?:<tr><th>Damage Type<\/th>\s*<td.*?>)(?<studentArmorType>[^<]*)(?:<\/td>)/gis.exec(text);
+
+	const {
+		groups: { studentCombatClass },
+	} = /(?:<tr><th>Combat Class<\/th>\s*<td.*?>)(?<studentCombatClass>[^<]*)(?:<\/td>)/gis.exec(text);
+
+	const studentAffinities = {
+		urban: undefined,
+		field: undefined,
+		indoors: undefined,
+	};
+	([
+		studentAffinities.urban,
+		studentAffinities.field,
+		studentAffinities.indoors
+	] = text.matchAll(/(\w+)(?: grade affinity)/gis).map(a => a[1]));
+
+	
+
+	
 
 	return {
 		studentName,
 		studentRarity,
 		studentProfileImg: `https://${studentProfileImg}`,
+		studentSchool,
+		studentGroup,
+		studentRole,
+		studentDamageType,
+		studentArmorType,
+		studentCombatClass,
+		studentAffinities,
 	};
 }
 
